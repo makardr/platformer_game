30653a45355f594298d4a6d8ff8db5afa258efc5
Makar <makardrozdik00@gmail.com>
2022-10-15 17:04:18 +0300

    Added first level, level renderer and camera movement

--- core/src/com/myplatformergdx/game/Screens/PlayScreen.java
+++ core/src/com/myplatformergdx/game/Screens/PlayScreen.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.myplatformergdx.game.MyPlatformerGame;
@@ -15,6 +18,12 @@ public class PlayScreen implements Screen {
     private OrthographicCamera gameCam;
     private Viewport gamePort;
 
+    private TmxMapLoader maploader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+
+
     public PlayScreen(MyPlatformerGame game) {
         this.game = game;
 
@@ -23,7 +32,12 @@ public class PlayScreen implements Screen {
         gameCam = new OrthographicCamera();
 //        Camera type
         gamePort = new FitViewport(MyPlatformerGame.V_WIDTH, MyPlatformerGame.V_HEIGHT, gameCam);
+
         hud = new Hud(game.batch);
+        maploader = new TmxMapLoader();
+        map = maploader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth()/2,gamePort.getWorldHeight()/2,0);
     }
 
     @Override
@@ -31,13 +45,38 @@ public class PlayScreen implements Screen {
 
     }
 
+
+
+
+    public void handleInput(float deltatime){
+        if(Gdx.input.isTouched())
+            gameCam.position.x += 100 * deltatime;
+    }
+
+//  global world update
+    public void update(float deltatime){
+        handleInput(deltatime);
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
+
+//    delta is called constantly
     @Override
     public void render(float delta) {
+        update(delta);
+
+//        clear screen
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        render world
+        renderer.render();
+
 //        Render what is shown in camera, draw hud
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
+
+
     }
 
     @Override
